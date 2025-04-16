f441e5cadca2efe1e85a330a70cf877e997f0f00
unknown <mateox463@gmail.com>
2025-04-16 19:00:33 -0300

    New: se añade primera configuracion de axios y se descarga dependencia

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const HomePage = () => {
   return (
     <>
-      <h1>Home</h1>
+      <h1 className="text-2xl font-bold text-gray-600">Home</h1>
       <a href="/admin/userPanel">Usuarios</a>
     </>
   );
