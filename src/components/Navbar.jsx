0d4c3372a2d947d4901424f8e914d6fdaea920d4
daysant14 <daymiles86@gmail.com>
2025-04-17 11:43:16 -0400

    Add  Navbar and Footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="bg-[#14599A] text-white py-4 !important">
+      <ul className="list-none flex gap-16 p-0 m-0">
+        <li><a href="#" className="text-white ">Home</a></li>
+        <li><a href="/admin/userPanel" className="text-white ">Usuarios</a></li>
+        <li><a href="#" className="text-white ">Postulaciones</a></li>
+        <li><a href="#" className="text-white ">Administrar Vacantes</a></li>
+        <li><a href="#" className="text-white">Administrar Usuarios</a></li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
+
+        
+
 const HomePage = () => {
   return (
     <>
       <h1 className="text-2xl font-bold text-gray-600">Home</h1>
-      <a href="/admin/userPanel">Usuarios</a>
+      
+      
     </>
   );
 };
