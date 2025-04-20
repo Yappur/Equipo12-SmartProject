188b0223f5aa9a28c0e5c6fc534e808d61f9d3a1
Carlos <carlosdaniellazo06@gmail.com>
2025-04-20 19:09:22 -0300

    logout y navbar

--- src/routes/RoutesViews.jsx
+++ src/routes/RoutesViews.jsx
@@ -1,27 +1,72 @@
 import { Routes, Route } from "react-router-dom";
-
+import { useEffect, useState } from "react";
+import { useLoginFirebase } from "../hooks/useLoginFirebase";
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/Auth/LoginPage";
 import App404 from "../pages/App404";
 import UserPanel from "../pages/AdminPages/UserPanel";
 import UserRegister from "../pages/Auth/UserRegister";
 import AdminView from "../pages/AdminPages/AdminView";
+import ProtectedRoute from "./ProtectedRoute";
+import Navbar from "../components/Navbar";
 
 const RoutesViews = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    !!localStorage.getItem("firebaseAuthToken")
+  );
+
+  useEffect(() => {
+    const handleStorageChange = () => {
+      setIsAuthenticated(!!localStorage.getItem("firebaseAuthToken"));
+    };
+
+    window.addEventListener("storage", handleStorageChange);
+
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+    };
+  }, []);
+
+  const { login, logout } = useLoginFirebase(setIsAuthenticated);
+
   return (
-    <Routes>
-      {/* Rutas Publicas */}
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
+    <>
+      <Navbar isAuthenticated={isAuthenticated} logout={logout} />
+      <Routes>
+        {/* Rutas Públicas */}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage login={login} />} />
 
-      {/* Rutas Privadas */}
-      <Route path="/admin" element={<AdminView />} />
-      <Route path="/admin/panelUsuarios" element={<UserPanel />} />
-      <Route path="/admin/crearUsuario" element={<UserRegister />} />
+        {/* Rutas Protegidas */}
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute isAuthenticated={isAuthenticated}>
+              <AdminView />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/admin/panelUsuarios"
+          element={
+            <ProtectedRoute isAuthenticated={isAuthenticated}>
+              <UserPanel />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/admin/crearUsuario"
+          element={
+            <ProtectedRoute isAuthenticated={isAuthenticated}>
+              <UserRegister />
+            </ProtectedRoute>
+          }
+        />
 
-      {/* Ruta 404 al final */}
-      <Route path="*" element={<App404 />} />
-    </Routes>
+        {/* Ruta 404 */}
+        <Route path="*" element={<App404 />} />
+      </Routes>
+    </>
   );
 };
 
