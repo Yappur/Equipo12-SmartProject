fd34424b82ae101b7071fa36c4a85516247ccd2f
unknown <mateox463@gmail.com>
2025-05-03 18:05:57 -0300

    fix: Se arregla error en loginPage

--- src/pages/Auth/LoginPage.jsx
+++ src/pages/Auth/LoginPage.jsx
@@ -1,9 +1,8 @@
-import { useState }  from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import logoLinkedin from "@/assets/img/logo-linkedin.png";
 import { useLoginFirebase } from "@/hooks/useLoginFirebase";
 import { useAuth } from "../../context/AuthContext";
-
 
 const LoginPage = () => {
   const [success, setSuccess] = useState(null);
@@ -21,29 +20,18 @@ const LoginPage = () => {
       setSuccess("¡Inicio de sesión exitoso!");
     }
   };
-  
+
   return (
     <div className="min-h-screen bg-[#00254B] flex items-center justify-center p-4 pt-16">
       <div className="bg-white/5 border border-white/10 rounded-xl overflow-hidden w-full max-w-5xl grid grid-cols-1 md:grid-cols-2 shadow-2xl backdrop-blur-md">
         <div className="p-8 md:p-12 text-white flex flex-col justify-center">
           <h2 className="text-3xl font-bold text-center mb-2">
             Bienvenido de nuevo a{" "}
-            <span className="text-[#008080] drop-shadow">Gestion</span>
+            <span className="text-[#008080] drop-shadow">TalentMatch</span>
           </h2>
           <p className="text-gray-300 text-center mb-6">Accede a tu cuenta</p>
 
-          <button className="flex items-center justify-center w-full border border-white/20 rounded-md py-2.5 px-4 text-white bg-[#14599A]/30 hover:bg-[#14599A]/50 transition mb-6">
-            <img
-              src={logoLinkedin}
-              alt="Logo LinkedIn"
-              className="w-5 h-5 mr-3"
-            />
-            Iniciar sesión con LinkedIn
-          </button>
-
           <div className="flex items-center text-gray-400 mb-6">
-            <div className="flex-grow h-px bg-white/20" />
-            <span className="px-3 text-sm">O</span>
             <div className="flex-grow h-px bg-white/20" />
           </div>
 
@@ -77,9 +65,6 @@ const LoginPage = () => {
                 <input type="checkbox" className="mr-2 accent-[#008080]" />
                 Recuérdame
               </label>
-              <a href="*" className="text-[#008080] hover:underline">
-                ¿Olvidaste tu contraseña?
-              </a>
             </div>
 
             <button
@@ -94,24 +79,37 @@ const LoginPage = () => {
               <p className="text-green-400 text-sm mt-2">{success}</p>
             )}
 
-{error && (
-  <div className="fixed top-10 left-1/2 transform -translate-x-1/2 z-50">
-    <div className="bg-red-50 border border-red-200 rounded-xl p-6 text-center shadow-lg w-[90vw] max-w-md animate-fade-in backdrop-blur-sm">
-      <div className="w-16 h-16 mx-auto mb-4 relative">
-        <div className="absolute inset-0 rounded-full bg-red-100 flex items-center justify-center">
-          <div className="w-10 h-10 rounded-full bg-red-500 flex items-center justify-center">
-            <span className="text-white text-2xl">✖</span>
-          </div>
-        </div>
-      </div>
-      <h2 className="text-red-700 font-semibold text-lg mb-1">Algun dato está incorrecto</h2>
-      <h2 className="text-red-700 font-semibold text-lg mb-1">Intentelo nuevamente o cambie la contraseña</h2>
-      <p className="text-red-600 text-sm">{error}</p>
-    </div>
-  </div>
-)}
-
-
+            {error && (
+              <div className="fixed top-10 left-1/2 transform -translate-x-1/2 z-50">
+                <div className="bg-red-50 border border-red-200 rounded-xl p-6 text-center shadow-lg w-[90vw] max-w-md animate-fade-in backdrop-blur-sm">
+                  <div className="w-16 h-16 mx-auto mb-4 relative">
+                    <div className="absolute inset-0 rounded-full bg-red-100 flex items-center justify-center">
+                      <div className="w-10 h-10 rounded-full bg-red-500 flex items-center justify-center">
+                        <span className="text-white text-2xl">✖</span>
+                      </div>
+                    </div>
+                  </div>
+                  <h2 className="text-red-700 font-semibold text-lg mb-1">
+                    Algun dato está incorrecto
+                  </h2>
+                  <h2 className="text-red-700 font-semibold text-lg mb-1">
+                    Intentelo nuevamente o cambie la contraseña
+                  </h2>
+                  <p className="text-red-600 text-sm">{error}</p>
+                </div>
+              </div>
+            )}
+            <div className="flex items-center text-gray-400 mt-2">
+              <div className="flex-grow h-px bg-white/20" />
+            </div>
+            <div>
+              <a
+                href="*"
+                className="text-[#008080] hover:underline flex items-center justify-center"
+              >
+                ¿Olvidaste tu contraseña?
+              </a>
+            </div>
           </form>
         </div>
 
