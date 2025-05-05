f932756c2958286db22506f3fd1086505ef03e83
unknown <mateox463@gmail.com>
2025-05-04 21:50:09 -0300

    se hace funcional el cambiar de contraseña

--- src/pages/Auth/RecoverAccount.jsx
+++ src/pages/Auth/RecoverAccount.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logoLogin from "@/assets/img/imgfigma.png";
+import { usePasswordReset } from "@/hooks/usePasswordReset";
 
-const ChangePassword = () => {
+const RecoverAccount = () => {
+  const [email, setEmail] = useState("");
+  const { resetPassword, error, cargando, success } = usePasswordReset();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    resetPassword(email);
+  };
+
   return (
     <div className="poppins min-h-screen bg-white flex items-center justify-center p-4 ">
       <div className="w-full max-w-5xl grid grid-cols-1 md:grid-cols-2 gap-8">
@@ -12,19 +21,29 @@ const ChangePassword = () => {
               Talent Match
             </h1>
 
-            <form className="space-y-4">
+            <form className="space-y-4" onSubmit={handleSubmit}>
               <div className="my-6">
                 <h2>Recupera tu contraseña con tu email de usuario</h2>
                 <input
                   type="email"
                   name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   className="w-full p-2.5 mt-1 bg-gray-200 border border-white/30 rounded-md placeholder-gray-600 text-sm focus:ring-2 focus:ring-[#14599A] focus:outline-none"
                   placeholder="Ingresa tu correo"
                   required
                 />
               </div>
+              {success && (
+                <p className="text-green-600 text-center">{success}</p>
+              )}
+              {error && <p className="text-red-600 text-center">{error}</p>}
+              {cargando && (
+                <p className="text-blue-600 text-center">Enviando...</p>
+              )}
               <button
                 type="submit"
+                disabled={cargando}
                 className="inter text-2xl w-1/3 flex items-center justify-center mx-auto bg-[#152d53] hover:bg-[#181f31] text-white font-semibold py-2.5 rounded-md transition shadow"
               >
                 Enviar
@@ -52,4 +71,4 @@ const ChangePassword = () => {
   );
 };
 
-export default ChangePassword;
+export default RecoverAccount;
