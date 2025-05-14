9bc882dc1d4d23d212876952c2b5151a7db91a6e
unknown <mateox463@gmail.com>
2025-05-14 00:33:17 -0300

    Se implementa responsive en Landing, se implementan animaciones con GSAP, se corrigen errores y se añade nuevo footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,43 +1,67 @@
-import { FaLinkedin, FaInstagram, FaTwitter } from "react-icons/fa";
+import React from "react";
+import { Link } from "react-router-dom";
+import { SiLinkedin } from "react-icons/si";
 
 const Footer = () => {
   return (
     <>
-      {/* <DemoSection /> */}
+      <footer className="bg-[#162C4D] text-white py-8 flex flex-col">
+        <div className="max-w-screen-3xl mx-30 md:px-8">
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-12 md:gap-20 xl:gap-40">
+            <div className="flex flex-col space-y-2">
+              <h2 className="font-semibold text-2xl tracking-wide">
+                {" "}
+                <span className="text-white">Talent </span>
+                <span className="text-orange-400 italic">Match</span>
+              </h2>
+              <p className="text-sm text-gray-300">
+                Todos los derechos reservados a Talent Match{" "}
+                {new Date().getFullYear()}
+              </p>
+            </div>
 
-      <footer className="bg-[#162C4D] text-white py-10 px-6">
-         <div className="flex flex-col items-center space-y-4">
-        <h1 className="text-xl font-medium">Nuestras redes</h1>
-        <div className="flex space-x-6 text-2xl">
-          <a
-            href="https://www.linkedin.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-500 hover:text-blue-400"
-          >
-            <FaLinkedin />
-          </a>
-          <a
-            href="https://www.instagram.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-pink-500 hover:text-pink-400"
-          >
-            <FaInstagram />
-          </a>
-          <a
-            href="https://www.twitter.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-sky-400 hover:text-sky-300"
-          >
-            <FaTwitter />
-          </a>
-        </div>
-      </div>
-        {/* Línea final */}
-        <div>
-          &copy; {new Date().getFullYear()} Talen Match - Foo Talent
+            <div className="flex flex-col space-y-2">
+              <h3 className="text-lg font-medium">Contáctanos</h3>
+              <p className="xl:text-sm text-gray-300 ">
+                Envíanos un mensaje a{" "}
+                <span className="md:text-xs sm:text-sm">
+                  talent.match.group@gmail.com
+                </span>
+              </p>
+            </div>
+
+            <div className="flex flex-col space-y-2">
+              <h3 className="text-lg font-medium ">Quiénes somos</h3>
+              <Link
+                to="/about"
+                className="text-sm text-gray-300 hover:text-blue-300 transition-colors"
+              >
+                Conócenos
+              </Link>
+            </div>
+
+            <div className="flex flex-col space-y-2">
+              <h3 className="text-lg font-medium">Seguimos</h3>
+              <div className="flex space-x-4">
+                <a
+                  href="https://www.linkedin.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-white hover:text-blue-400 transition-colors"
+                  aria-label="LinkedIn"
+                >
+                  <span className="flex items-center gap-1.5 text-gray-300">
+                    <SiLinkedin className="text-xl text-blue-500 bg-white rounded-xs" />{" "}
+                    Linkedin
+                  </span>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Línea final */}
+          <div className="mt-8 pt-4 text-sm text-gray-300">
+            Parte del programa Smart Project - Foo Talent Group
+          </div>
         </div>
       </footer>
     </>
